b47dd740e976f5eea9a756aec016d972115ceb66
RBESYAD1 <rbesyad1@ford.com>
2019-07-03 10:56:01 +0300

    Addedd pipelines

---
+++ build/.eslinttest.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'env': {
+    'browser': true
+  },
+  'globals': {
+    'process': 'readonly',
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  'rules': {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  'parserOptions': {
+    'parser': 'babel-eslint'
+  },
+  'plugins': [
+    'vue'
+  ]
+};
